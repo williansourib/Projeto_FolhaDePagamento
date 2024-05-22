4742dacbbd4f68b170e27a2c48f3eeaf2191547d
williansourib <willianribeiro709@gmail.com>
2024-05-22 18:21:22 -0300

    Adicionado Calculo Imposto de renda

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const calcularINSS = require('./calculo_inss');
+const calcularImpostoRenda = require('./calculo_imposto_renda');
 
-const salarioBruto = 4000.00;
+const salarioBruto = 3500.00;
 const valorINSS = calcularINSS(salarioBruto);
+const valorImpostoRenda = calcularImpostoRenda(salarioBruto);
 
-console.log(`Para um salário bruto de R$ ${salarioBruto}, o valor a ser pago de INSS é R$ ${valorINSS.toFixed(2)}`);
+console.log(`Para um salário bruto de R$ ${salarioBruto.toFixed(2)}, o valor a ser pago de INSS é R$ ${valorINSS.toFixed(2)}`);
+console.log(`Para um salário bruto de R$ ${salarioBruto.toFixed(2)}, o valor a ser pago de Imposto de Renda é R$ ${valorImpostoRenda.toFixed(2)}`);
