107998de99c842a0d3a89e77b0746df72809e6f9
williansourib <willianribeiro709@gmail.com>
2024-05-22 17:33:49 -0300

    Adicionado Calculo inss

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-console.log("Projeto folha_pagamento está rodando!");
+const calcularINSS = require('./calculo_inss');
+
+const salarioBruto = 4000.00;
+const valorINSS = calcularINSS(salarioBruto);
+
+console.log(`Para um salário bruto de R$ ${salarioBruto}, o valor a ser pago de INSS é R$ ${valorINSS.toFixed(2)}`);
